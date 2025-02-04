77229cf6558dfa710560eab7aa8887664407c648
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-02-04 23:11:21 +0100

    commit

--- ai-content-generator/app/dashboard/_components/TemplateCard.tsx
+++ ai-content-generator/app/dashboard/_components/TemplateCard.tsx
@@ -4,9 +4,10 @@ import { TEMPLATE } from './TemplateListSection'
 
 function TemplateCard(item:TEMPLATE) {
   return (
-    <div>
+    <div className='p-5 shadow-md rounded-md border bg-white flex flex-col gap-3 hover:scale-105 cursor-pointer'>
         <Image src={item.icon} alt='icon' width={50} height={50}/>
-        <h2>{item.name}</h2>
+        <h2 className='font-medium text-lg'>{item.name}</h2>
+        <p className='text-gray-500 lne-champ-3'>{item.desc}</p>
     </div>
   )
 }
